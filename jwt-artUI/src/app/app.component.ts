6e8840650864104f8042a0bcfb4d1ac1d5a93e3d
harshsinha21 <harshw.sinha21@gmail.com>
2025-03-09 14:47:56 -0400

    updated redirect routes and login forms using angular CLI and bootstrap

--- jwt-artUI/src/app/app.component.ts
+++ jwt-artUI/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, RouterModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, RouterModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- jwt-artUI/src/app/login/login.component.ts
+++ jwt-artUI/src/app/login/login.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  constructor() {}
+  ngOnInit(): void {  
+  }
+  login(loginForm: NgForm) {
+    console.log("Form is submitted");
+    console.log(loginForm.value);
+    // console.log("Username:", loginForm.value.userName);
+    // console.log("Password:", loginForm.value.userPassword);
+  }
 }
+
+
