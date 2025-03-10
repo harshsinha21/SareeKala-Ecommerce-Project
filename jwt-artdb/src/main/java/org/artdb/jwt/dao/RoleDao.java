c3f64bfeaf0d7b0b748fb35cdff3655a463e922d
harshsinha21 <harshw.sinha21@gmail.com>
2025-03-10 15:58:12 -0400

    updating name of project

--- jwt-artdb/src/main/java/org/artdb/jwt/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package org.artdb.jwt.dao;
-
-import org.artdb.jwt.entity.Role;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleDao extends CrudRepository<Role, String> {
-}
--- jwt-artdb/src/main/java/org/artdb/jwt/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package org.artdb.jwt.dao;
-
-import org.artdb.jwt.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends CrudRepository<User, String> {
-}
